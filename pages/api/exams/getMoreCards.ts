f3c8e6d52e9e7a56391fe8ad7964cc3e3bbdff6a
jrmygrd <jeremy.gourdeau@edu.devinci.fr>
2020-12-08 19:42:38 +0100

    scorect

--- pages/api/exams/getMoreCards.ts
+++ pages/api/exams/getMoreCards.ts
@@ -8,15 +8,14 @@ export default async function (req:NextApiRequest, res: NextApiResponse) {
     const prisma = new PrismaClient({log:["query"]});
 
     try{
-        const cards = await prisma.$queryRaw`select cards.id,question,tip,bad_options,answer,cards.fk_user,streak,nbGood,nbBad from cards
+        const cards = await prisma.$queryRaw`select cards.id,question,tip,bad_options,answer,cards.fk_user,streak,streakct,nbGood,nbBad from cards
         join cards_users cu on cards.id = cu.fk_card
     where id in (select fk_cardID from cards_stacks where fk_stackID in
             (select fk_stackID from exam_list where exam_list.fk_examId = ${req.body.fk_deck}))
     and cu.fk_user = '1w7K30BqJFTR6rJLKdAP9aasoKM2'
     order by cu.score,cu.score_ct 
     limit 15;`   
-    
-    console.log(cards)
+ 
     res.status(201);//created
     res.json(cards);
  
